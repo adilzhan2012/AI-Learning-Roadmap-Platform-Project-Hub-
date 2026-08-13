f08a643e56024624e91d8e4fc4081211aea20c1e
daniil170 <daniilivakin30@gmail.com>
2026-08-14 02:59:08 +0500

    chore: remove unused Skeleton and XPProgressBar components

--- src/components/admin/ui/Skeleton.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export default function Skeleton({ className = "" }) {
-  return (
-    <div className={`bg-zinc-800/50 rounded-lg animate-pulse ${className}`}></div>
-  );
-}
